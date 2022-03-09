e2c0ae59a6661ff968d1fe80259965752763fa88
lllilllilllilili <hik3562@hufs.ac.kr>
2022-03-09 22:23:06 +0900

    feat: 자바빈즈 user 생성

---
+++ src/user/User.java
@@ -0,0 +1,50 @@
+package user;
+
+public class User {
+    private String userID;
+    private String userPassword;
+    private String userName;
+    private String userGender;
+    private String userEmail;
+
+//    자바 빈즈 구성
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserGender(String userGender) {
+        this.userGender = userGender;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserGender() {
+        return userGender;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+}
